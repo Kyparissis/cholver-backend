9925052b960e3054d4fddf6c7c647b78ca290c9f
Kyparissis Kyparissis <65011914+Kyparissis@users.noreply.github.com>
2023-11-06 16:53:46 +0200

    Add NodeJS server
    Initial commit of NodeJS auto-generated server from Swagger

---
+++ service/RatingService.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+/**
+ * Give a rating to a user
+ * FR11 - The user must be able to rate other users. 
+ *
+ * body UserID_rate_body 
+ * userID String 
+ * no response value expected for this operation
+ **/
+exports.userUserIDRatePUT = function(body,userID) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
