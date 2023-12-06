1876d80526636d8bffd508c28dfb207c7ed9666b
Kyparissis Kyparissis <65011914+Kyparissis@users.noreply.github.com>
2023-12-06 15:49:12 +0200

    Add more tests for AD entity

--- tests/ad_kyparissis.test.js
+++ tests/ad_kyparissis.test.js
@@ -23,6 +23,10 @@ test.after.always((t) => {
     t.context.server.close();
 });
 
+
+// -=-=-=-=-=-=-=-=-= GET user/{userID}/ad -=-=-=-=-=-=-=-=-=
+
+
 // Test GET all ads from specific user (by calling the function)
 test("GET all ads from specific user by function", async t => {
     // Define path parameters
@@ -47,7 +51,7 @@ test("GET all ads from specific user", async t => {
     // Define path parameters
     const userID = 6;
     // Make GET request to server
-    const { body, statusCode } = await t.context.got(`user/${userID}/ad`);
+    const { body, statusCode } = await t.context.got.get(`user/${userID}/ad`);
     // Assert success status code
     t.is(statusCode, 200);
     // Assert that we get two entries (body must be an array) 
@@ -62,6 +66,10 @@ test("GET all ads from specific user", async t => {
     t.is(firstAd.title, "title");
     t.is(firstAd.userID, 6);
 });
+
+
+// -=-=-=-=-=-=-=-=-= GET user/{userID}/ad/{adID} -=-=-=-=-=-=-=-=-=
+
 
 // Test GET the interested users for an ad (by calling the function)
 test("GET the interested users for an ad by function", async t => {
@@ -93,7 +101,7 @@ test("GET the interested users for an ad", async t => {
     const userID = 6;
     const adID = 1;
     // Make GET request to server
-    const { body, statusCode } = await t.context.got(`user/${userID}/ad/${adID}`);
+    const { body, statusCode } = await t.context.got.get(`user/${userID}/ad/${adID}`);
     // Assert success status code
     t.is(statusCode, 200);
     // Assert that we get two entries (body must be an array)
@@ -112,6 +120,10 @@ test("GET the interested users for an ad", async t => {
     t.is(firstProfile.email, "email");
     t.is(firstProfile.age, 6);
 });
+
+
+// -=-=-=-=-=-=-=-=-= POST user/{userID}/ad -=-=-=-=-=-=-=-=-=
+
 
 // Test POST a new ad (by calling the function)
 test("POST a new ad by function", async t => {
@@ -145,4 +157,208 @@ test("POST a new ad", async t => {
     t.is(Object.keys(body).length, 1);
     // Assert that we get the expected body
     t.is(body.message, "message");
+});
+
+// Test POST a new ad | Integer in title in request body (by sending a POST request to the server)
+test("POST a new ad | Integer in title in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": 313131231,
+        "adDescription": "string"
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.title should be string");
+});
+
+// Test POST a new ad | Integer in adDescription in request body (by sending a POST request to the server)
+test("POST a new ad | Integer in adDescription in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": "string",
+        "adDescription": 31313123131
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.adDescription should be string");
+});
+
+// Test POST a new ad | Integer in adDescription,title in request body (by sending a POST request to the server)
+test("POST a new ad | Integer in adDescription,title in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": 131312313131,
+        "adDescription": 31313123131
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.title should be string, request.body.adDescription should be string");
+});
+
+// Test POST a new ad | Integer in adDescription,title in request body (by sending a POST request to the server)
+test("POST a new ad | Request body isn't JSON", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": "string",
+        "adDescription": "string"
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 415);
+    // Assert error message
+    t.is(error.response.body.message, "unsupported media type undefined");
+});
+
+// Test POST a new ad | Multiple titles in request body (by sending a POST request to the server)
+test("POST a new ad | Multiple titles in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": ["string", "string2"],
+        "adDescription": "string",
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.title should be string");
+});
+
+// Test POST a new ad | Multiple titles, adDescriptions in request body (by sending a POST request to the server)
+test("POST a new ad | Multiple titles, adDescriptions in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": ["string", "string2"],
+        "adDescription": ["string", "string2"],
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.title should be string, request.body.adDescription should be string");
+});
+
+// Test POST a new ad | Multiple adDescriptions in request body (by sending a POST request to the server)
+test("POST a new ad | Multiple adDescriptions in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": "string",
+        "adDescription": ["string", "string2"],
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.adDescription should be string");
+});
+
+// Test POST a new ad | Multiple adDescriptions in request body (by sending a POST request to the server)
+test("POST a new ad | No request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 415);
+    // Assert error message
+    t.is(error.response.body.message, "unsupported media type undefined");
+});
+
+// Test POST a new ad | No request body (by sending a POST request to the server)
+test("POST a new ad | Undefined as request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: undefined}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 415);
+    // Assert error message
+    t.is(error.response.body.message, "unsupported media type undefined");
+});
+
+// Test POST a new ad | No request body (by sending a POST request to the server)
+test("POST a new ad | Null title in request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = {
+        "title": null,
+        "adDescription": "string"
+    };
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body.title should be string");
+});
+
+// Test POST a new ad | Null adDescription in request body (by sending a POST request to the server)
+test("POST a new ad | Array of correct bodies request body", async t => {
+    // Define path parameters
+    const userID = 6;
+    const postBody = [{
+        "title": "string",
+        "adDescription": "string"
+    },{
+        "title": "string",
+        "adDescription": "string"
+    }];
+    // Send POST request to server
+    const error = await t.throwsAsync(async () => {
+        await t.context.got.post(`user/${userID}/ad`, {json: postBody}),
+        { instanceof: got.HTTPError }
+    });
+    // Assert error status code
+    t.is(error.response.statusCode, 400);
+    // Assert error message
+    t.is(error.response.body.message, "request.body should be object");
 });
