c5cafd5f1449c8a8fe6f1d319f6e89eec569eb11
Marianna Giannakopoulou <105949772+gmarianna@users.noreply.github.com>
2023-12-06 19:25:12 +0200

    Add tests for Ad-Entity

---
+++ controllers/User.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var User = require('../service/UserService');
+
+module.exports.userGET = function userGET (req, res, next, keyword) {
+  User.userGET(keyword)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userPOST = function userPOST (req, res, next, body) {
+  User.userPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userUserIDDELETE = function userUserIDDELETE (req, res, next, userID) {
+  User.userUserIDDELETE(userID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userUserIDGET = function userUserIDGET (req, res, next, userID) {
+  User.userUserIDGET(userID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userUserIDPUT = function userUserIDPUT (req, res, next, body, userID) {
+  User.userUserIDPUT(body, userID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.userUserIDProfile_picturePUT = function userUserIDProfile_picturePUT (req, res, next, userID) {
+  User.userUserIDProfile_picturePUT(userID)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
